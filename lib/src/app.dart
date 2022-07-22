bec3c41a8e814413035658de93d41a3bc76c770d
SamTobias <13008789+SamTobias@users.noreply.github.com>
2022-07-22 16:13:49 -0300

    Refactor initial code

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_restoration/src/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
