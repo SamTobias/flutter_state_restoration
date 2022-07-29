d17f31393d242ca6bfcfd18291ee529a42f47db7
SamTobias <13008789+SamTobias@users.noreply.github.com>
2022-07-29 16:24:13 -0300

    Add bottom navigation example

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -11,11 +11,11 @@ class App extends StatelessWidget {
       title: 'Flutter State Restoration',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomePage(),
-      restorationScopeId: "app", /// Root restoration scope
+      restorationScopeId: "app", // Root restoration scope
       onGenerateRoute: (routeSettings) {
         if (routeSettings.name == "/second") {
           return MaterialPageRoute(builder: (context) {
-            /// Arguments object can't be a custom class, it needs to a "primitive type" (Int, Bool, String, Map, List...)
+            // Arguments object can't be a custom class, it needs to a "primitive type" (Int, Bool, String, Map, List...)
             final arguments = routeSettings.arguments as Map;
             final title = arguments["title"] as String;
 
--- lib/src/second_page.dart
+++ lib/src/second_page.dart
@@ -11,18 +11,12 @@ class SecondPage extends StatefulWidget {
 
 class _SecondPageState extends State<SecondPage> with RestorationMixin {
   final RestorableInt _counter = RestorableInt(0);
-  final RestorableTextEditingController _textEditingController = RestorableTextEditingController();
-  final RestorableBool _checked = RestorableBool(false);
-  final RestorableString _text = RestorableString("");
-  int _selectedIndex = 0;
-  static const List<Widget> _widgetOptions = <Widget>[
-    // TODO: Add listview as page
-    Placeholder()
-  ];
+  final RestorableInt _selectedIndex = RestorableInt(0);
+  final List<String> items = List<String>.generate(10000, (i) => 'Item $i');
 
   void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      _selectedIndex.value = index;
     });
   }
 
@@ -43,65 +37,17 @@ class _SecondPageState extends State<SecondPage> with RestorationMixin {
           Navigator.of(context).pop(widget.title);
           return false;
         },
-        child: ListView(
-          padding: const EdgeInsets.all(16),
-          restorationId: "card_list",
-
-          /// Restore scroll position
-          children: <Widget>[
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(32),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Text(
-                      'You have pushed the button this many times:',
-                    ),
-                    Text(
-                      '${_counter.value}',
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(32),
-                child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-                  TextField(
-                    controller: _textEditingController.value,
-                    decoration: const InputDecoration(label: Text("Insert some text here")),
-                    onSubmitted: (String text) {
-                      setState(() {
-                        _text.value = text;
-                      });
-                    },
-                  ),
-                  Text("Submitted text: ${_text.value}"),
-                  CheckboxListTile(
-                    title: const Text("Check me!"),
-                    value: _checked.value,
-                    onChanged: (checked) {
-                      setState(() {
-                        _checked.value = checked ?? false;
-                      });
-                    },
-                  ),
-                ]),
-              ),
-            ),
-            // TODO: Add more cards
-          ],
-        ),
+        child: [
+          MainTab(counter: _counter.value),
+          ListTab(items: items),
+        ].elementAt(_selectedIndex.value),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Main"),
-          BottomNavigationBarItem(icon: Icon(Icons.question_mark), label: "Secondary")
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: "List")
         ],
-        currentIndex: _selectedIndex,
+        currentIndex: _selectedIndex.value,
         onTap: _onItemTapped,
       ),
       floatingActionButton: FloatingActionButton(
@@ -117,12 +63,106 @@ class _SecondPageState extends State<SecondPage> with RestorationMixin {
 
   @override
   void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
-    /// Register properties that will be restored
+    // Register properties that will be restored
     registerForRestoration(_counter, "counter");
+    registerForRestoration(_selectedIndex, "tab_index");
+  }
+}
+
+class ListTab extends StatelessWidget {
+  final List<String> items;
+
+  const ListTab({Key? key, required this.items}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      // Add restorationId to a ListView to restore its scroll position
+      restorationId: "card_list",
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(items[index]),
+        );
+      },
+    );
+  }
+}
+
+class MainTab extends StatefulWidget {
+  final int counter;
+
+  const MainTab({Key? key, required this.counter}) : super(key: key);
+
+  @override
+  State<MainTab> createState() => _MainTabState();
+}
+
+class _MainTabState extends State<MainTab> with RestorationMixin {
+  final RestorableTextEditingController _textEditingController = RestorableTextEditingController();
+  final RestorableBool _checked = RestorableBool(false);
+  final RestorableString _text = RestorableString("");
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('You have pushed the button this many times:'),
+                Text(
+                  '${widget.counter}',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              TextField(
+                controller: _textEditingController.value,
+                decoration: const InputDecoration(label: Text("Insert some text here")),
+                onSubmitted: (String text) {
+                  setState(() {
+                    _text.value = text;
+                  });
+                },
+              ),
+              Text("Submitted text: ${_text.value}"),
+              CheckboxListTile(
+                title: const Text("Check me!"),
+                value: _checked.value,
+                onChanged: (checked) {
+                  setState(() {
+                    _checked.value = checked ?? false;
+                  });
+                },
+              ),
+            ]),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  String? get restorationId => "main_tab";
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    // Register properties that will be restored
     registerForRestoration(_checked, "checkbox");
     registerForRestoration(_text, "text");
 
-    /// Instead of below, you can just set restorationId on TextField
+    // Instead of below, you can just set restorationId on TextField
     registerForRestoration(_textEditingController, "text_field");
   }
 }
